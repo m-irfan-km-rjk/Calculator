d4bf57bf440eb3b2d9a2fa4201aa1f9e64ad6a74
unknown <mirfankm915@gmail.com>
2024-12-15 18:55:06 +0530

    modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,31 @@
 var txt = document.querySelector("#input-text");
 var ans = false;
 
+document.addEventListener('keydown',(e) => {
+    var key = e.key;
+    console.log(key);
+    if(key == '/') {
+        addToCalc('÷');
+    } else if(key == '*') {
+        addToCalc('x');
+    } else if(key == 'Backspace') {
+        addToCalc('ce');
+    } else if(key == '=' || key == 'Enter') {
+        addToCalc('eq');
+    } else if(key == '+') {
+        addToCalc('+');
+    } else if(key == '-') {
+        addToCalc('-');
+    } else if(!isNaN(parseInt(key))) {
+        addToCalc(key);
+    }
+});
+
 function addToCalc(item) {
     if(item === "ac") {
         txt.value = "0";
     } else if(item === "ce") {
-        if(item.value.length == 1) {
+        if(txt.value.length == 1) {
             txt.value = "0";
         } else {
             txt.value = txt.value.slice(0,-1);
@@ -15,8 +35,12 @@ function addToCalc(item) {
         ans = true;
     } else {
         if(ans == true) {
+            if(isNaN(parseInt(item))) {
+                txt.value += item;
+            } else {
+                txt.value = item;
+            }
             ans = false;
-            txt.value = item;
         } else {
             if(txt.value != "0") {
                 txt.value += item;
